a773a92eb41c65466450cd98dff7a30cb5805b57
Blucbo <skochkobo@gmail.com>
2016-12-19 23:04:05 +0200

    Get and print users

--- Test/script.js
+++ Test/script.js
@@ -1,6 +1,20 @@
 var app = angular.module("app", []);
 
+
 var MainController = function($scope, $http) {
+
+  var onUsersComplete = function(response) {
+    $scope.users = response.data;
+  };
+
+  var onError = function(reason) {
+    $scope.error = "Could not fetch the data.";
+  };
+
+  $scope.getUsers = function(){
+      $http.get("http://jsonplaceholder.typicode.com/users/")
+        .then(onUsersComplete, onError);
+  };
 
 };
 
