d353bbaa0ef5a264bdd38141cd4b87b6cb3f3980
Blucbo <skochkobo@gmail.com>
2016-12-19 23:18:19 +0200

    Add sort

--- Test/script.js
+++ Test/script.js
@@ -15,7 +15,19 @@ var MainController = function($scope, $http) {
       $http.get("http://jsonplaceholder.typicode.com/users/")
         .then(onUsersComplete, onError);
   };
+  navigator.geolocation.getCurrentPosition(function(position){
+      var lat = position.coords.latitude;
+      var lng = position.coords.longitude;
+      $scope.$apply(function(){
+        $scope.lat = lat;
+        $scope.lng = lng;
+      });
+   });
+  $scope.dist = function(geo){
+    
+  }
 
+  $scope.usersSortOrder = "+name";
 };
 
 app.controller("MainController", ["$scope", "$http", MainController]);
